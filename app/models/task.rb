76c2d688a7de9d9f2a42bd3e7ccc68a021c6f20d
Arthur Herminie <arthur.herminie@gmail.com>
2022-01-31 13:11:21 +0100

    started working on task's crud actions and tasks view

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,24 @@
 class Task < ApplicationRecord
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description, :status)
+  end
 end
